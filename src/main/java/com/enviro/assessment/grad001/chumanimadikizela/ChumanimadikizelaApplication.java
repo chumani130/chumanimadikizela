35b32f536b3a167ee958433022e01580a3be96e0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-04 14:42:08 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/ChumanimadikizelaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.chumanimadikizela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChumanimadikizelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChumanimadikizelaApplication.class, args);
+	}
+
+}
