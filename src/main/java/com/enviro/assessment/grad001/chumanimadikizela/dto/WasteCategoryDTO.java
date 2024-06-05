3efde22baf6e251aa6f8cf127e10c3415afa3682
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 12:51:56 +0200

    Update WasteCategoryDTO.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/dto/WasteCategoryDTO.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/dto/WasteCategoryDTO.java
@@ -1,14 +1,13 @@
 package com.enviro.assessment.grad001.chumanimadikizela.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class WasteCategoryDTO {
     private Long id;
     private String name;
