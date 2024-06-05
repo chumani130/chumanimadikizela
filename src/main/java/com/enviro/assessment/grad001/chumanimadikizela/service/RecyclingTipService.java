8e74f0ecd5d895e427071d0c9667d0c7bd391491
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 14:16:01 +0200

    Update RecyclingTipService.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipService.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipService.java
@@ -1,4 +1,18 @@
 package com.enviro.assessment.grad001.chumanimadikizela.service;
 
-public class RecyclingTipService {
+import com.enviro.assessment.grad001.chumanimadikizela.dto.RecyclingTipDTO;
+
+import java.util.List;
+
+public interface RecyclingTipService {
+
+    public List<RecyclingTipDTO> getAllRecyclingTips();
+
+    public RecyclingTipDTO getRecyclingTiById(Long id);
+
+    public RecyclingTipDTO createRecyclingTip(RecyclingTipDTO recyclingTipDTO);
+
+    public RecyclingTipDTO updateRecyclingTip(Long id, RecyclingTipDTO recyclingTipDTO);
+
+    void DeleteRecyclingTipById(Long id);
 }
