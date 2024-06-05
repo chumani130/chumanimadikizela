411936cce9be25caeb14652d5a8c931a596f0ea0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 18:45:21 +0200

    Update RecyclingTipServiceImpl.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipServiceImpl.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipServiceImpl.java
@@ -1,34 +1,61 @@
 package com.enviro.assessment.grad001.chumanimadikizela.service;
 
 import com.enviro.assessment.grad001.chumanimadikizela.dto.RecyclingTipDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.dto.WasteCategoryDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.model.RecyclingTip;
+import com.enviro.assessment.grad001.chumanimadikizela.model.WasteCategory;
+import com.enviro.assessment.grad001.chumanimadikizela.repository.RecyclingTipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class RecyclingTipServiceImpl implements RecyclingTipService {
+    @Autowired
+    private RecyclingTipRepository recyclingTipRepository;
     @Override
     public List<RecyclingTipDTO> getAllRecyclingTips() {
-        return null;
+        List<RecyclingTip> recyclingTips = recyclingTipRepository.findAll();
+        return recyclingTips.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public RecyclingTipDTO getRecyclingTiById(Long id) {
-        return null;
+        Optional<RecyclingTip> recyclingTip = recyclingTipRepository.findById(id);
+        return recyclingTip.map(this::convertToDTO).orElse(null);
     }
 
     @Override
     public RecyclingTipDTO createRecyclingTip(RecyclingTipDTO recyclingTipDTO) {
-        return null;
+        RecyclingTip recyclingTip = new RecyclingTip();
+        recyclingTip.setTip(recyclingTip.getTip());
+        RecyclingTip savedTip = recyclingTipRepository.save(recyclingTip);
+        return convertToDTO(savedTip);
     }
 
     @Override
     public RecyclingTipDTO updateRecyclingTip(Long id, RecyclingTipDTO recyclingTipDTO) {
-        return null;
+        Optional<RecyclingTip> optionalRecyclingTip = recyclingTipRepository.findById(id);
+        if (optionalRecyclingTip.isPresent()) {
+            RecyclingTip recyclingTip = optionalRecyclingTip.get();
+            recyclingTip.setTip(recyclingTipDTO.getTip());
+            RecyclingTip savedTip = recyclingTipRepository.save(recyclingTip);
+            return convertToDTO(savedTip);
+        } else {
+            return null;
+        }
     }
-
     @Override
     public void DeleteRecyclingTipById(Long id) {
-
+        recyclingTipRepository.deleteById(id);
+    }
+    private RecyclingTipDTO convertToDTO(RecyclingTip recyclingTip) {
+        RecyclingTipDTO dto = new RecyclingTipDTO();
+        dto.setId(recyclingTip.getId());
+        dto.setTip(recyclingTip.getTip());
+        return dto;
     }
 }
