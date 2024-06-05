81b190f31a986a2c255eaf6dac158080ba207b32
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 10:57:26 +0200

    Create DisposalGuideLineController.java

---
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/controller/DisposalGuideLineController.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.chumanimadikizela.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DisposalGuideLineController {
+}
