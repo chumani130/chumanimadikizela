5902b4b9d718ff41f619afd43439e9d3cb5814f0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 14:15:46 +0200

    Create DisposalGuideLineServiceImpl.java

---
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/DisposalGuideLineServiceImpl.java
@@ -0,0 +1,34 @@
+package com.enviro.assessment.grad001.chumanimadikizela.service;
+
+import com.enviro.assessment.grad001.chumanimadikizela.dto.DisposalGuideLineDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DisposalGuideLineServiceImpl implements DisposalGuideLineService {
+    @Override
+    public List<DisposalGuideLineDTO> getAllGuideLines() {
+        return null;
+    }
+
+    @Override
+    public DisposalGuideLineDTO getGuideLineById(Long id) {
+        return null;
+    }
+
+    @Override
+    public DisposalGuideLineDTO createGuideLine(DisposalGuideLineDTO disposalGuideLineDTO) {
+        return null;
+    }
+
+    @Override
+    public DisposalGuideLineDTO updateGuideLine(Long id, DisposalGuideLineDTO disposalGuideLineDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteGuideLine(Long id) {
+
+    }
+}
