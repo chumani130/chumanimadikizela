c916e0b5d031238f9bf375486193a817c0458214
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 12:50:53 +0200

    Update WasteCategory.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/model/WasteCategory.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/model/WasteCategory.java
@@ -2,16 +2,15 @@ package com.enviro.assessment.grad001.chumanimadikizela.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@Getter
+@Setter
 @Table(name="Waste Category")
 public class WasteCategory {
     @Id
