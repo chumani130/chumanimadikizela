4f85f4fd63d06549fae19d6a23f2a7bd8b7c5853
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 14:16:15 +0200

    Create RecyclingTipServiceImpl.java

---
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipServiceImpl.java
@@ -0,0 +1,34 @@
+package com.enviro.assessment.grad001.chumanimadikizela.service;
+
+import com.enviro.assessment.grad001.chumanimadikizela.dto.RecyclingTipDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecyclingTipServiceImpl implements RecyclingTipService {
+    @Override
+    public List<RecyclingTipDTO> getAllRecyclingTips() {
+        return null;
+    }
+
+    @Override
+    public RecyclingTipDTO getRecyclingTiById(Long id) {
+        return null;
+    }
+
+    @Override
+    public RecyclingTipDTO createRecyclingTip(RecyclingTipDTO recyclingTipDTO) {
+        return null;
+    }
+
+    @Override
+    public RecyclingTipDTO updateRecyclingTip(Long id, RecyclingTipDTO recyclingTipDTO) {
+        return null;
+    }
+
+    @Override
+    public void DeleteRecyclingTipById(Long id) {
+
+    }
+}
