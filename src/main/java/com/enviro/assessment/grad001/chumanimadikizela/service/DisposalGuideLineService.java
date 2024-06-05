bd4129f7d4fb9d3922df72164115c8be1fb30fab
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 14:15:31 +0200

    Update DisposalGuideLineService.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/DisposalGuideLineService.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/DisposalGuideLineService.java
@@ -1,4 +1,19 @@
 package com.enviro.assessment.grad001.chumanimadikizela.service;
 
-public class DisposalGuideLineService {
+import com.enviro.assessment.grad001.chumanimadikizela.dto.DisposalGuideLineDTO;
+
+import java.util.List;
+
+public interface DisposalGuideLineService {
+    public List<DisposalGuideLineDTO> getAllGuideLines();
+
+    public DisposalGuideLineDTO getGuideLineById(Long id);
+
+    public DisposalGuideLineDTO createGuideLine(DisposalGuideLineDTO disposalGuideLineDTO);
+
+    public DisposalGuideLineDTO updateGuideLine(Long id, DisposalGuideLineDTO disposalGuideLineDTO);
+
+    void deleteGuideLine(Long id);
+
+
 }
