86dac90208f9747d6ff50227b5c1371115c9a07b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 12:51:06 +0200

    Update WasteCategoryServiceImpl.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/WasteCategoryServiceImpl.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/WasteCategoryServiceImpl.java
@@ -1,90 +1,88 @@
-package com.enviro.assessment.grad001.chumanimadikizela.service;
-
-import com.enviro.assessment.grad001.chumanimadikizela.dto.WasteCategoryDTO;
-import com.enviro.assessment.grad001.chumanimadikizela.model.WasteCategory;
-import com.enviro.assessment.grad001.chumanimadikizela.repository.WasteCategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-
-@Service
-public class WasteCategoryServiceImpl implements WasteCategoryService{
-
-        @Autowired
-    private WasteCategoryRepository wasteCategoryRepository;
-    @Override
-    public List<WasteCategoryDTO> getAllCategories() {
-        return null;
-    }
-
-    @Override
-    public WasteCategoryDTO getCategoryById(Long id) {
-        return null;
-    }
-
-    @Override
-    public WasteCategoryDTO createCategory(WasteCategoryDTO wasteCategoryDTO) {
-        return null;
-    }
-
-    @Override
-    public WasteCategoryDTO update(Long id, WasteCategoryDTO wasteCategoryDTO) {
-        return null;
-    }
-
-    @Override
-    public void deleteCategoryById(Long id) {
-
-    }
-}
-
+//package com.enviro.assessment.grad001.chumanimadikizela.service;
+//
+//import com.enviro.assessment.grad001.chumanimadikizela.dto.WasteCategoryDTO;
+//import com.enviro.assessment.grad001.chumanimadikizela.model.WasteCategory;
+//import com.enviro.assessment.grad001.chumanimadikizela.repository.WasteCategoryRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Objects;
+//
 //@Service
-//public class WasteCategoryService {
-
+//public class WasteCategoryServiceImpl implements WasteCategoryService {
 //
+//    @Autowired
+//    private WasteCategoryRepository wasteCategoryRepository;
+//
+//    // Save operation
+//
+//    // Read operation
+//
+//    // Update operation
+//
+//
+//    // Delete operation
+//
+//
+//    @Override
 //    public List<WasteCategoryDTO> getAllCategories() {
-//        return wasteCategoryRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
+//            List<WasteCategoryDTO> wasteCategoryDTOS = new ArrayList<>();
+//            List<WasteCategory> wasteCategories = wasteCategoryRepository.findAll();
+//
+//            for (WasteCategory wasteCategory : wasteCategories) {
+//                wasteCategoryDTOS.add(WasteCategoryDTO.builder()
+//                        .id(wasteCategory.getId())
+//                        .name(wasteCategory.getName())
+//                        .build());
+//            }
+//
+//            return wasteCategoryDTOS;
+//        }
 //    }
 //
+//    @Override
 //    public WasteCategoryDTO getCategoryById(Long id) {
-//        Optional<WasteCategory> wasteCategory = wasteCategoryRepository.findById(id);
-//        return wasteCategory.map(this.convertToDTO).orElse(null);
+//        return null;
 //    }
 //
+//    @Override
 //    public WasteCategoryDTO createCategory(WasteCategoryDTO wasteCategoryDTO) {
-//        WasteCategory wasteCategory = new WasteCategory();
-//        wasteCategory.setName(wasteCategoryDTO.getName());
-//        WasteCategory savedCategory = wasteCategoryRepository.save(wasteCategory);
-//        return convertToDTO(savedCategory);
+//        WasteCategory wasteCategory = WasteCategory.builder()
+//                .id(wasteCategoryDTO.getId())
+//                .name(wasteCategoryDTO.getName())
+//                .build();
+//        wasteCategory = wasteCategoryRepository.save(wasteCategory);
+//        return WasteCategoryDTO.builder()
+//                .id(wasteCategory.getId())
+//                .name(wasteCategory.getName())
+//                .build();
 //    }
 //
+//    @Override
 //    public WasteCategoryDTO update(Long id, WasteCategoryDTO wasteCategoryDTO) {
-//        Optional<WasteCategory> wasteCategory = wasteCategoryRepository.findById(id);
-//        if ( wasteCategory.isPresent()) {
-//            WasteCategory updatedCategory = wasteCategory.get();
-//            updatedCategory.setName(wasteCategoryDTO.getName());
-//            wasteCategoryRepository.save(updatedCategory);
-//            return convertToDTO(updatedCategory);
-//        } else {
-//            return null;
+//        WasteCategory wasteCategoryDB = wasteCategoryRepository.findById(id).orElseThrow(() -> new RuntimeException("WasteCategory not found"));
+//
+//        if (Objects.nonNull(wasteCategoryDTO.getName()) &&!"".equals(wasteCategoryDTO.getName())) {
+//            wasteCategoryDB.setName(wasteCategoryDTO.getName());
 //        }
+//        if (Objects.nonNull(wasteCategoryDTO.getName()) &&!"".equals(wasteCategoryDTO.getName())) {
+//            wasteCategoryDB.setName(wasteCategoryDTO.getName());
+//        }
+//        if (Objects.nonNull(wasteCategoryDTO.getName()) &&!"".equals(wasteCategoryDTO.getName())) {
+//            wasteCategoryDB.setName(wasteCategoryDTO.getName());
+//        }
+//
+//        wasteCategoryDB = wasteCategoryRepository.save(wasteCategoryDB);
+//        return WasteCategoryDTO.builder()
+//                .id(wasteCategoryDB.getId())
+//                .name(wasteCategoryDB.getName())
+//                .build();
 //    }
 //
-//    public boolean deleteCategory(Long id) {
-//        if (wasteCategoryRepository.existsById(id)) {
-//            wasteCategoryRepository.deleteById(id);
-//            return true;
-//        } else {
-//            return false;
-//        }
-//
-//        private WasteCategoryDTO convertToDTO(WasteCategory wasteCategory) {
-//            WasteCategoryDTO dto = new WasteCategoryDTO();
-//            dto.setId(wasteCategory.getId());
-//            dto.setName(wasteCategory,getName());
-//            return dto;
-//        }
+//    @Override
+//    public void deleteCategoryById(Long id) {
+//        wasteCategoryRepository.deleteById(id);
 //    }
 //}
