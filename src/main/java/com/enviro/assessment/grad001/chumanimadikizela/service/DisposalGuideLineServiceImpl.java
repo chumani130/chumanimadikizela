e7e450076ab1d59607e6a690443a652fe4279ecb
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 18:45:13 +0200

    Update DisposalGuideLineServiceImpl.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/DisposalGuideLineServiceImpl.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/service/DisposalGuideLineServiceImpl.java
@@ -1,34 +1,62 @@
 package com.enviro.assessment.grad001.chumanimadikizela.service;
 
 import com.enviro.assessment.grad001.chumanimadikizela.dto.DisposalGuideLineDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.dto.RecyclingTipDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.model.DisposalGuideLine;
+import com.enviro.assessment.grad001.chumanimadikizela.model.RecyclingTip;
+import com.enviro.assessment.grad001.chumanimadikizela.repository.DisposalGuideLineRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DisposalGuideLineServiceImpl implements DisposalGuideLineService {
+
+    private DisposalGuideLineRepository disposalGuideLineRepository;
     @Override
     public List<DisposalGuideLineDTO> getAllGuideLines() {
-        return null;
+        List<DisposalGuideLine> disposalGuideLines = disposalGuideLineRepository.findAll();
+        return disposalGuideLines.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
     public DisposalGuideLineDTO getGuideLineById(Long id) {
-        return null;
+        Optional<DisposalGuideLine> disposalGuideLine = disposalGuideLineRepository.findById(id);
+        return disposalGuideLine.map(this::convertToDTO).orElse(null);
     }
 
     @Override
     public DisposalGuideLineDTO createGuideLine(DisposalGuideLineDTO disposalGuideLineDTO) {
-        return null;
+        DisposalGuideLine disposalGuideLine = new DisposalGuideLine();
+        disposalGuideLine.setGuideline(disposalGuideLine.getGuideline());
+        DisposalGuideLine savedGuideLine = disposalGuideLineRepository.save(disposalGuideLine);
+        return convertToDTO(savedGuideLine);
     }
 
     @Override
     public DisposalGuideLineDTO updateGuideLine(Long id, DisposalGuideLineDTO disposalGuideLineDTO) {
-        return null;
+        Optional<DisposalGuideLine> optionalDisposalGuideLine = disposalGuideLineRepository.findById(id);
+        if (optionalDisposalGuideLine.isPresent()) {
+            DisposalGuideLine disposalGuideLine = optionalDisposalGuideLine.get();
+            disposalGuideLine.setGuideline(disposalGuideLineDTO.getGuideline());
+            DisposalGuideLine savedGuideLine = disposalGuideLineRepository.save(disposalGuideLine);
+            return convertToDTO(disposalGuideLine);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void deleteGuideLine(Long id) {
+        disposalGuideLineRepository.deleteById(id);
+    }
 
+    private DisposalGuideLineDTO convertToDTO(DisposalGuideLine disposalGuideLine) {
+        DisposalGuideLineDTO dto = new DisposalGuideLineDTO();
+        dto.setId(disposalGuideLine.getId());
+        dto.setGuideline(disposalGuideLine.getGuideline());
+        return dto;
     }
 }
