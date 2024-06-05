c560438d82c21385fa3f824bdc44a55ee80f6212
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-05 18:43:11 +0200

    Update RecyclingTipController.java

--- src/main/java/com/enviro/assessment/grad001/chumanimadikizela/controller/RecyclingTipController.java
+++ src/main/java/com/enviro/assessment/grad001/chumanimadikizela/controller/RecyclingTipController.java
@@ -1,7 +1,47 @@
 package com.enviro.assessment.grad001.chumanimadikizela.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.enviro.assessment.grad001.chumanimadikizela.dto.RecyclingTipDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.dto.WasteCategoryDTO;
+import com.enviro.assessment.grad001.chumanimadikizela.service.RecyclingTipService;
+import com.enviro.assessment.grad001.chumanimadikizela.service.WasteCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/recycling-tip")
 public class RecyclingTipController {
+    @Autowired
+    private RecyclingTipService recyclingTipService;
+
+    // retrieves a list of all waste categories  or READ
+    @GetMapping
+    public List<RecyclingTipDTO> getAllTips() {
+        return recyclingTipService.getAllRecyclingTips();
+    }
+
+    // read by id
+    @GetMapping("/{id}")
+    public RecyclingTipDTO getTipsById(@PathVariable Long id) {
+        return recyclingTipService.getRecyclingTiById(id);
+    }
+
+    // create
+    @PostMapping("/")
+    public RecyclingTipDTO createRecyclingTip(@RequestBody RecyclingTipDTO recyclingTipDTO) {
+        return recyclingTipService.createRecyclingTip(recyclingTipDTO);
+    }
+
+    // update
+    @PutMapping("/{id}")
+    public RecyclingTipDTO updateRecyclingTip(@PathVariable Long id,
+                                              @RequestBody RecyclingTipDTO recyclingTipDTO) {
+        return recyclingTipService.updateRecyclingTip(id, recyclingTipDTO);
+    }
+
+    // delete
+    public void deleteRecycleTipById(@PathVariable Long id) {
+        recyclingTipService.getRecyclingTiById(id);
+    }
 }
